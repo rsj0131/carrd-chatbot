53fd7cc5ca3bcff7d22b11ec44050d32acc200b4
rsj0131 <65953630+rsj0131@users.noreply.github.com>
2024-12-10 22:57:29 +0800

    Update login.js

--- api/twitter/login.js
+++ api/twitter/login.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     // Store the code_verifier in a secure HTTP-only cookie
     res.setHeader(
         "Set-Cookie",
-        `code_verifier=${codeVerifier}; HttpOnly; Path=/; Max-Age=300; Secure; SameSite=Strict`
+        `code_verifier=${codeVerifier}; HttpOnly; Path=/; Max-Age=300; Secure; SameSite=Lax`
     );
 
     // Generate the corresponding code_challenge
