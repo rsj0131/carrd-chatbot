7c83ba332e22b95dbff7e0105ee4fc9ae1c6c01b
rsj0131 <65953630+rsj0131@users.noreply.github.com>
2024-12-10 17:56:22 +0800

    Update login.js

--- api/twitter/login.js
+++ api/twitter/login.js
@@ -1,3 +1,13 @@
+import crypto from "crypto";
+
+function generateCodeVerifier() {
+    return crypto.randomBytes(32).toString("base64url");
+}
+
+function generateCodeChallenge(verifier) {
+    return crypto.createHash("sha256").update(verifier).digest("base64url");
+}
+
 import { URLSearchParams } from "url";
 
 export default async function handler(req, res) {
