10efd749d79e31e7c06a34454053de09aaaffa88
rsj0131 <65953630+rsj0131@users.noreply.github.com>
2024-12-19 10:36:38 +0800

    Update mistral.js

--- api/mistral.js
+++ api/mistral.js
@@ -228,7 +228,7 @@ export default async function handler(req, res) {
         const knowledgeResponse = await getAnswer(message);
         
         const characterDetails = await getCharacterDetails(characterId);
-        const presetHistory = await loadPresetHistory(process.env.PRESET_CHAT_ID);
+        const presetHistory = " "; /*await loadPresetHistory(process.env.PRESET_CHAT_ID);*/
         const characterName = characterDetails.name || "assistant";
 
         const tools = await fetchFunctions();
