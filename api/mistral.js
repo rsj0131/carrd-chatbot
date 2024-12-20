a224a80e175a0e8ade718902e6b71d01848e74b9
rsj0131 <65953630+rsj0131@users.noreply.github.com>
2024-12-20 13:47:43 +0800

    Update mistral.js

--- api/mistral.js
+++ api/mistral.js
@@ -307,12 +307,12 @@ export default async function handler(req, res) {
         }
 
         const messages = [
-            { role: "system", content: dynamicSystemMessage },
+            /*{ role: "system", content: dynamicSystemMessage },*/
             /*...presetHistory,*/
-            ...history.flatMap(entry => [
+            /*...history.flatMap(entry => [
                 { role: "user", content: entry.userMessage },
                 { role: "assistant", content: entry.botReply },
-            ]),
+            ]),*/
             { role: "user", content: message },
         ];
         
