69ffe077485068519b9d8776de507cbd146c67cb
rsj0131 <65953630+rsj0131@users.noreply.github.com>
2024-12-31 17:02:36 +0800

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -36,22 +36,6 @@ const safetySettings = [
         category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
         threshold: HarmBlockThreshold.BLOCK_NONE,
     },
-    {
-        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-    },
-    {
-        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
-    },
-    {
-        category: HarmCategory.HARM_CATEGORY_CIVIC_INTEGRITY,
-        threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-    },
 ];
 
 function getPricingForModel(model) {
